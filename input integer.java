f89a6b98cccea1d4ad03b69b8cf41a77a22a86c2
nehabmuhammed <128158357+nehabmuhammed@users.noreply.github.com>
2024-08-20 11:58:36 +0530

    Add files via upload

---
+++ input integer.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+        
+        
+        
+        
+        
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+        
+        
+    }
+}
